d5388c240889284d88b188a4083c6f3fd24d5cfc
Joel Schecheleski <joel.jsp@gmail.com>
2017-06-08 15:22:05 -0300

    Alterações em meus arquivos

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-class Message{
-
+class Message {
+  constructor(text = '', created = Date.now()){
+    this.text - text;
+    this.created = created;
+  }
+  get created(){
+    return this._created;
+  }
+  set created(created){
+    if(!created || isNaN(created)){
+      throw new Error('invalid created');
+    }
+    this._created = created;
+  }
+  toString(){
+    return `Message created at: ${this.created} - Text: ${this.text}`;
+  }
 }
